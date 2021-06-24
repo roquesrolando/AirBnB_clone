f505a28da7aeb2ba20ab4d8046facb04cc0f2608
csoriano2832 <2832@holbertonschool.com>
2021-06-24 19:01:40 +0000

    add basemodel

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+
+"""
+from datetime import datetime
+import uuid
+
+class BaseModel():
+    """
+
+    """
+    def __init__(self, *args, **kwargs):
+        """
+
+        """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+
+        """
+        return ("[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+
+        """
+        dictionary = dict(self.__dict__)
+
+        dictionary["__class__"] = __class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return dictionary
