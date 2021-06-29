a25026532ce41658672fa06bc9e32880a0161378
csoriano2832 <2832@holbertonschool.com>
2021-06-29 14:07:45 +0000

    add documentation

--- console.py
+++ console.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" """
+"""This module contains the entry point of the command interpreter"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ """
+    """This class defines the actions the user can do inside of the console"""
     prompt = '(hbnb) '
 
     def do_quit(self, line):
@@ -12,11 +12,11 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, line):
-        """ """
+        """EOF command to exit the program\n"""
         return True
 
     def emptyline(self):
-        """ """
+        """Overrides default emptyline method"""
         pass
 
 if __name__ == '__main__':
