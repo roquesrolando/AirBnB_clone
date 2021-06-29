178ba547bf94a41f50a73e59639b90f01f6ef59e
csoriano2832 <2832@holbertonschool.com>
2021-06-29 14:30:26 +0000

    fix pep8 errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-This module contains the BaseModel class for all instances
-"""
-import uuid
+""" This module contains the BaseModel class for all instances """
 from datetime import datetime
 import models
+import uuid
+
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
-    """
-    BaseModel class
-    """
+    """ BaseModel class """
     def __init__(self, *args, **kwargs):
-        """
-        Initializer BaseModel attributes
-        """
+        """ Initialize BaseModel attributes """
+        fmt = DATE_FORMAT
+
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -22,28 +20,29 @@ class BaseModel():
             models.storage.new(self)
         else:
             del kwargs['__class__']
-            kwargs["created_at"] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["created_at"] = datetime.strptime(kwargs['created_at'], fmt)
+            kwargs["updated_at"] = datetime.strptime(kwargs['updated_at'], fmt)
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def __str__(self):
-        """
-        String of a class
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ String of a class """
+        class_name = self.__class__.__name__
+
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         """ Saves an object to JSON file """
+
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Return a dictionary with key and values of __dict__
-        """
+        """ Return a dictionary with key and values of __dict__ """
+
         todic = self.__dict__.copy()
         todic["__class__"] = self.__class__.__name__
         todic["created_at"] = self.created_at.isoformat()
         todic["updated_at"] = self.updated_at.isoformat()
+
         return todic
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Manage file storage for BaseModel
-"""
+""" Manage file storage for BaseModel """
 import json
 from .. import classes
 
+
 class FileStorage:
-    """
-    Serielizes and deserialize to a JSON file and from a JSON file
-    """
+    """ Serializes and deserializes to a JSON file and from a JSON file """
     __file_path = 'file.json'
     __objects = {}
 
@@ -21,22 +18,23 @@ class FileStorage:
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
-        """ Serializes objects JSON file """
+        """ Serializes objects to JSON file """
         new_dic = {}
+
         with open(self.__file_path, mode='w', encoding='UTF-8') as a_dict:
             for key, value in self.__objects.items():
                 new_dic[key] = value.to_dict()
             json.dump(new_dic, a_dict)
 
     def reload(self):
-        """
-        Deserialies from JSON file
-        """
+        """ Deserializes objects from JSON file """
         try:
             filename = self.__file_path
             with open(filename, mode='r', encoding='UTF-8') as a_dict:
                 b_dict = json.load(a_dict)
             for key in b_dict:
-                self.__objects[key] = classes[b_dict[key]["__class__"]](**b_dict[key])
+                self.__objects[key] = \
+                        classes[b_dict[key]["__class__"]](**b_dict[key])
+
         except FileNotFoundError:
             pass
