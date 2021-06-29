7c8e56cd2b0fb20067e33f06709f66f8d13af3df
roquesrolando <roquesrolando@hotmail.com>
2021-06-29 21:00:45 +0000

    updated classes

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,11 @@ from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.review import review
+from models.review import Review
 from models.place import Place
 from models.base_model import BaseModel
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"BaseModel": BaseModel, "User": User, "State": State,
+           "Amenity": Amenity, "City": City, "Review": Review, "Place": Place}
 
 try:
     from models.engine.file_storage import FileStorage
