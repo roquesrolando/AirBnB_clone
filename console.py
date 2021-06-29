bbf524e108b303035da0e0a9dc68c04b85a9b3e7
roquesrolando <roquesrolando@hotmail.com>
2021-06-29 20:01:11 +0000

    works

--- console.py
+++ console.py
@@ -91,6 +91,32 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, line):
         """Updates an instance based on the class name and id\n"""
+        args = line.split()
+        objs = storage.all()
+
+        try:
+            key = args[0] + "." + args[1]
+        except:
+            pass
+        if len(line) == 0:
+            print("** class name missing **")
+
+        elif args[0] in classes:
+            if len(args) < 2:
+                print("** instance id missing **")
+            elif len(args) < 3:
+                print("** atttribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            elif key not in objs:
+                print("** no instance found **")
+            else:
+                for clas, value in objs.items():
+                    if args[1] == value.id:
+                        setattr(value, args[2], args[3])
+
+        else:
+            print("** class doesn't exist **")
 
     def emptyline(self):
         """Overrides default emptyline method"""
