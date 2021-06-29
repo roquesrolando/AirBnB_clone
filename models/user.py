3997434fb776d2b0483615a61e56e490d0f16461
roquesrolando <roquesrolando@hotmail.com>
2021-06-29 20:33:26 +0000

    documentation

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
+Defines User
 """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """
+    User subclass of BaseModel
     """
     email = ""
     password = ""
