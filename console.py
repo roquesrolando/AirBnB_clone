2093212e71b7a7b1e8f58f95bdc63d61c4c74881
csoriano2832 <2832@holbertonschool.com>
2021-06-29 14:01:36 +0000

    add console 0.0.1

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, line):
+        """ """
+        return True
+
+    def emptyline(self):
+        """ """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
